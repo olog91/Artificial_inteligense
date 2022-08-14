8a0e632c8309b210475eba03b26b01184d8ba8cc
olog91 <olog91@gmail.com>
2022-08-14 20:55:02 +0300

    add React and slog

---
+++ src/assets/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Slogan () {
+
+
+    return (
+
+        <>
+            <h1>Hello World</h1>
+            <p>this is my page</p>
+        </>
+
+    )
+
+
+}
+
